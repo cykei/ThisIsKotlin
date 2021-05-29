64065ac80d6f965cda24c883c9440b0293f0c92d
cykei <kelle111@naver.com>
2021-05-29 17:22:51 +0900

    리스너 & 인텐트 테스트

--- BasicSyntax/app/src/main/java/com/cykei/basicsyntax/MainActivity.kt
+++ BasicSyntax/app/src/main/java/com/cykei/basicsyntax/MainActivity.kt
@@ -1,14 +1,35 @@
 package com.cykei.basicsyntax
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import androidx.core.widget.addTextChangedListener
+import com.cykei.basicsyntax.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
 
         Log.d("BasicSyntax", "로그를 출력합니다. method = Log.d")
+
+        binding.editTextTextPersonName. addTextChangedListener {
+            if(it.toString().length >= 0) {
+                Log.d("hello","hello")
+            }
+        }
+
+        val intent = Intent(this, SubActivity::class.java)
+        val listener = object : View.OnClickListener{
+            override fun onClick(v: View?) {
+                Log.d("리스너", "출력되었습니다.")
+                startActivity(intent)
+            }
+        }
+
+        binding.button.setOnClickListener(listener)
     }
 }
