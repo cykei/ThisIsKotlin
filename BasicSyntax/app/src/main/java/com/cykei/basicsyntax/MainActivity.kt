15ec5fffb4db79ef180bdb6b1a3778c328a3d0df
cykei <kelle111@naver.com>
2021-05-29 17:55:11 +0900

    SubActivity -> MainActivity 로 값 돌려받기

--- BasicSyntax/app/src/main/java/com/cykei/basicsyntax/MainActivity.kt
+++ BasicSyntax/app/src/main/java/com/cykei/basicsyntax/MainActivity.kt
@@ -5,20 +5,21 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
+import android.widget.Toast
 import androidx.core.widget.addTextChangedListener
 import com.cykei.basicsyntax.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    val binding by lazy {ActivityMainBinding.inflate(layoutInflater)}
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         Log.d("BasicSyntax", "로그를 출력합니다. method = Log.d")
 
-        binding.editTextTextPersonName. addTextChangedListener {
-            if(it.toString().length >= 0) {
-                Log.d("hello","hello")
+        binding.editTextTextPersonName.addTextChangedListener {
+            if (it.toString().length >= 0) {
+                Log.d("hello", "hello")
             }
         }
 
@@ -26,13 +27,27 @@ class MainActivity : AppCompatActivity() {
         intent.putExtra("from1", "Hello Bundle")
         intent.putExtra("from2", 2021)
 
-        val listener = object : View.OnClickListener{
+        val listener = object : View.OnClickListener {
             override fun onClick(v: View?) {
                 Log.d("리스너", "출력되었습니다.")
-                startActivity(intent)
+
+                // requestCode (두번째 인자) : 여러개의 버튼이 있고, 각 버튼으로 열리는 액티비티가 전부 메세지를 반환하는 경우, 각 액티비티를 구분하기위해 임의로 지정해주는 숫자
+                startActivityForResult(intent, 99)
             }
         }
 
         binding.button.setOnClickListener(listener)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == RESULT_OK) {
+            when (requestCode) {
+                99 -> {
+                    val message = data?.getStringExtra("returnValue")
+                    Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                }
+            }
+        }
     }
 }
--- BasicSyntax/app/src/main/java/com/cykei/basicsyntax/SubActivity.kt
+++ BasicSyntax/app/src/main/java/com/cykei/basicsyntax/SubActivity.kt
@@ -1,5 +1,6 @@
 package com.cykei.basicsyntax
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cykei.basicsyntax.databinding.ActivitySubBinding
@@ -12,6 +13,13 @@ class SubActivity : AppCompatActivity() {
 
         binding.to1.text = intent.getStringExtra("from1")
         binding.to2.text = intent.getIntExtra("from2",0).toString()
+
+        binding.btnClose.setOnClickListener {
+            val returnIntent = Intent() // 돌려줄때는 대상을 지정하지 않아도 된다.
+            returnIntent.putExtra("returnValue", binding.editMessage.text.toString())
+            setResult(RESULT_OK, returnIntent) // setResult() : 해당 함수를 실행하면 호출한 액티비티로 값이 전달된다.
+            finish() // 액티비티를 종료한다.
+        }
 
     }
 }
