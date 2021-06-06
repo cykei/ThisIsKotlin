30354b6983152a85f08f17c5e25ef63d0f11c645
cykei <kelle111@naver.com>
2021-06-06 13:07:41 +0900

    뷰페이저랑 탭 레이아웃 이랑 연결

--- ViewPager/app/src/main/java/com/cykei/viewpager/MainActivity.kt
+++ ViewPager/app/src/main/java/com/cykei/viewpager/MainActivity.kt
@@ -3,6 +3,7 @@ package com.cykei.viewpager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.cykei.viewpager.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
     val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
@@ -14,5 +15,10 @@ class MainActivity : AppCompatActivity() {
         val adapter = FragmentAdapter(this)
         adapter.fragmentList = fragmentList
         binding.viewPager.adapter = adapter
+
+        val tabTitles = listOf<String>("A","B","C","D")
+        TabLayoutMediator(binding.tabLayout, binding.viewPager) { tab, position ->
+            tab.text = tabTitles[position]
+        }.attach()
     }
 }
