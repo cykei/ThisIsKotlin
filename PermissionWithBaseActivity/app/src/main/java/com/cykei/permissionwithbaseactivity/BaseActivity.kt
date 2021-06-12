ddebc53f22e59a6a23a99aa08e81a9d98c3a13dc
cykei <kelle111@naver.com>
2021-06-13 07:28:42 +0900

    권한예제- BaseAcivity를 상속받아서 권한 처리

---
+++ PermissionWithBaseActivity/app/src/main/java/com/cykei/permissionwithbaseactivity/BaseActivity.kt
@@ -0,0 +1,39 @@
+package com.cykei.permissionwithbaseactivity
+
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+abstract class BaseActivity :AppCompatActivity(){
+    abstract fun permissionGranted(requestCode : Int)
+    abstract fun permissionDenied(requestCode : Int)
+    fun requirePermissions(permissions: Array<String>, requestCode: Int){
+        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.M){
+            // 핸드폰의 버전이 안드로이드 6.0 (마시멜로우) 미만이면 permissionGranted()를 호출한다.
+            permissionGranted(requestCode)
+        }else{
+            val isAllPermissionsGranted = permissions.all {
+                checkSelfPermission(it) == PackageManager.PERMISSION_GRANTED
+            }
+            if (isAllPermissionsGranted) {
+                permissionGranted(requestCode)
+            } else {
+                ActivityCompat.requestPermissions(this, permissions, requestCode)
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if(grantResults.all { it == PackageManager.PERMISSION_GRANTED }) {
+            permissionGranted(requestCode)
+        } else {
+            permissionDenied(requestCode)
+        }
+    }
+}
