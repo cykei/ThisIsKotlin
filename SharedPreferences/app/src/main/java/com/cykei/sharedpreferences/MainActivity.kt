6a26f1b9b4e9c0321127fcd56122a435777c3702
cykei <kelle111@naver.com>
2021-06-15 22:12:28 +0900

    PreferenceScreen 설정값 사용하기

--- SharedPreferences/app/src/main/java/com/cykei/sharedpreferences/MainActivity.kt
+++ SharedPreferences/app/src/main/java/com/cykei/sharedpreferences/MainActivity.kt
@@ -2,10 +2,20 @@ package com.cykei.sharedpreferences
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.preference.PreferenceManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val shared = PreferenceManager.getDefaultSharedPreferences(this)
+        val checkBoxValue = shared.getBoolean("key_add_shortcut", false)
+        val switchValue = shared.getBoolean("key_switch_on",false)
+        val name = shared.getString("key_edit_name","")
+        val selected = shared.getString("key_set_item","")
+
+        Log.d("preferences","cb: ${checkBoxValue}, sw: ${switchValue}, name: ${name}, selected: ${selected}")
     }
 }
