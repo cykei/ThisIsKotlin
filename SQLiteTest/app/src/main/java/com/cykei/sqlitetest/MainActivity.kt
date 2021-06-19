41f97fcee6413f6a633215a585d23c65e7ab6fb3
cykei <kelle111@naver.com>
2021-06-19 09:35:55 +0900

    MainActivity에 코드 조합 - 메모 저장 & 보기 기능 완성

--- SQLiteTest/app/src/main/java/com/cykei/sqlitetest/MainActivity.kt
+++ SQLiteTest/app/src/main/java/com/cykei/sqlitetest/MainActivity.kt
@@ -2,10 +2,34 @@ package com.cykei.sqlitetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.cykei.sqlitetest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater)}
+    val helper = SqliteHelper(this, "memo", 1)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        val adapter = RecyclerAdapter()
+        adapter.listData.addAll(helper.selectMemo())
+        binding.recyclerMemo.adapter = adapter
+        binding.recyclerMemo.layoutManager = LinearLayoutManager(this)
+
+        binding.buttonSave.setOnClickListener {
+            if(binding.editMemo.text.toString().isNotEmpty()) {
+                val memo = Memo(null, binding.editMemo.text.toString(), System.currentTimeMillis())
+                helper.insertMemo(memo)
+
+                // 어댑터의 데이터를 모두 초기화한다. (새로운 데이터를 입력했으니까 그 데이터까지 뷰에 보여주기위해서는 초기화를 해줘야함)
+                adapter.listData.clear()
+                adapter.listData.addAll(helper.selectMemo())
+                adapter.notifyDataSetChanged()
+
+                // 마지막으로 EditText의 메모 내용을 지운다.
+                binding.editMemo.setText("")
+            }
+        }
     }
 }
