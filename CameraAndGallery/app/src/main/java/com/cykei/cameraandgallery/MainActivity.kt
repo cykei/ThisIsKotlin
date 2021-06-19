f4c1b72f1a9df97505bf450e36f2de9c41a9e6a9
cykei <kelle111@naver.com>
2021-06-19 18:52:46 +0900

    카메라로 찍은 사진의 원본 화면이 이미지뷰에 보여진다.

--- CameraAndGallery/app/src/main/java/com/cykei/cameraandgallery/MainActivity.kt
+++ CameraAndGallery/app/src/main/java/com/cykei/cameraandgallery/MainActivity.kt
@@ -1,13 +1,19 @@
 package com.cykei.cameraandgallery
 
 import android.Manifest
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.widget.Toast
 import com.cykei.cameraandgallery.databinding.ActivityMainBinding
+import java.lang.Exception
+import java.text.SimpleDateFormat
 
 class MainActivity : BaseActivity() {
 
@@ -32,9 +38,51 @@ class MainActivity : BaseActivity() {
             requirePermissions(arrayOf(Manifest.permission.CAMERA), PERM_CAMERA)
         }
     }
+
+    // 사진을 찍었을 때, 내 폰에 사진이 저장될 위치
+    var realUri: Uri? = null
+
+    // 3. 카메라에 찍은 사진을 저장하기 위한 URI를 넘겨준다.
     fun openCamera() {
         val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        createImageUri(newFileName(),"image/jpg")?.let{ uri ->
+            // createImageUri() 가 null이 아니면 작동한다.
+            realUri = uri
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, realUri)
+            startActivityForResult(intent, REQ_CAMERA)
+        }
         startActivityForResult(intent, REQ_CAMERA)
+    }
+
+    // 원본 이미지를 저장할 Uri를 MediaStore(데이터베이스)에 생성하는 메서드
+    // Uri (내가 이미지를 저장할 수 있는 위치)를 생성해서 반환해준다.
+    fun createImageUri(filename: String, mimeType: String): Uri? {
+        val values = ContentValues()
+        values.put(MediaStore.Images.Media.DISPLAY_NAME, filename)
+        values.put(MediaStore.Images.Media.MIME_TYPE, mimeType)
+
+        return contentResolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values)
+    }
+
+    fun newFileName() : String {
+        val sdf = SimpleDateFormat("yyyyMMdd_HHmmss")
+        val filename = sdf.format(System.currentTimeMillis())
+        return "${filename}.jpg"
+    }
+
+    fun loadBitmap(photoUri: Uri): Bitmap? {
+        //var image: Bitmap? = null
+        try {
+            return if(Build.VERSION.SDK_INT > 27) {
+                val source = ImageDecoder.createSource(contentResolver, photoUri)
+                ImageDecoder.decodeBitmap(source)
+            } else {
+                MediaStore.Images.Media.getBitmap(contentResolver, photoUri)
+            }
+        }catch(e: Exception){
+            e.printStackTrace()
+        }
+        return null
     }
 
     override fun permissionGranted(requestCode: Int) {
@@ -54,6 +102,7 @@ class MainActivity : BaseActivity() {
         }
     }
 
+    // 4. 카메라를 찍은 후에 호출된다.
     // openCamera() 로 카메라를 실행시키면 그 반환값이 여기로 넘어오게된다. 이미지는 3번째 파라미터인 data에 들어있다.
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
@@ -61,8 +110,15 @@ class MainActivity : BaseActivity() {
             when(requestCode) {
                 REQ_CAMERA -> {
                     // 미리보기 이미지가 가져와진다.그냥 get() 하면 오브젝트로 가져오기 때문에 비트맵으로 이미지를 변환해줘야 한다.
-                    val bitmap = data?.extras?.get("data") as Bitmap
-                    binding.imagePreview.setImageBitmap(bitmap)
+//                    val bitmap = data?.extras?.get("data") as Bitmap
+//                    binding.imagePreview.setImageBitmap(bitmap)
+
+                    realUri?.let{ uri ->
+                        val bitmap = loadBitmap(uri)
+                        binding.imagePreview.setImageBitmap(bitmap)
+
+                        realUri = null
+                    }
                 }
             }
         }
