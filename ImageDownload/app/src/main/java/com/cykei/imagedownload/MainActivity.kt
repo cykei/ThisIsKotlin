5d42edb51083bedc69cd4c4c5ab3f30d472a9114
cykei <kelle111@naver.com>
2021-06-26 22:10:50 +0900

    이미지 다운로드 버튼 완성

--- ImageDownload/app/src/main/java/com/cykei/imagedownload/MainActivity.kt
+++ ImageDownload/app/src/main/java/com/cykei/imagedownload/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.cykei.imagedownload
 
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import java.net.URL
+
+
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.cykei.imagedownload.databinding.ActivityMainBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
+    val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
+
+        binding.buttonDownload.setOnClickListener {
+            CoroutineScope(Dispatchers.Main).launch {
+                binding.progress.visibility = View.VISIBLE
+                val url = binding.editUrl.text.toString()
+                val bitmap = withContext(Dispatchers.IO) {
+                    loadImage(url)
+                    // 로드 이미지가 완료될때까지 다음 줄이 실행되지 않는다.
+                }
+                binding.imagePreview.setImageBitmap(bitmap)
+                binding.progress.visibility = View.GONE
+            }
+        }
     }
+}
+
+suspend fun loadImage(imageUrl: String): Bitmap {
+    val url = URL(imageUrl)
+    val stream = url.openStream()
+    return BitmapFactory.decodeStream(stream)
 }
